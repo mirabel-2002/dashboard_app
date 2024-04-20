3675fbf5060c4ecb27c83f9af4838bbbc84c00ad
mirabel-2002 <152276206+mirabel-2002@users.noreply.github.com>
2024-04-20 14:05:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import seaborn as sns
-#import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
